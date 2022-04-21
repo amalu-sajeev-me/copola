d976c1ff92c9248e1b86a2c386769d415f7e39cb
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-04-21 17:02:07 +0530

    404 route fix

--- src/routers/default.router.ts
+++ src/routers/default.router.ts
@@ -1,7 +1,9 @@
 import { RequestHandler } from "express";
+import { scream } from "../utils/APIError";
 
-const defaultRoute: RequestHandler = (req, res, next) => {
-    next(new Error("Page Not Found"));
+const defaultRoute: RequestHandler = () => {
+    const notFoundError = [404, "Page Not Found"] as const;
+    scream(...notFoundError);
 };
 
 export { defaultRoute };
